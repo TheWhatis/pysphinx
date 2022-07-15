96e17896a1ef17776c7ffdb227d33985dfa882e0
TheWhatis <anton-gogo@mail.ru>
2022-07-15 14:36:37 +0700

    Добавил новые функции для тестов

--- tests/test-functions.py
+++ tests/test-functions.py
@@ -1,3 +1,12 @@
+from abc import ABCMeta, abstractmethod
+
+
+def decorator(func):
+    def __wrapper__():
+        print("This do decorator")
+        return func()
+    return __wrapper__
+
 
 class MyClass:
     """This is my description of class"""
@@ -21,6 +30,10 @@ class MyClass:
         """Static Method Description"""
         pass
 
+    @decorator
+    def myDecoratedMethod(self, i: int, la: int = 3):
+        pass
+
 
 class MyClass2:
     """This is my description of class"""
@@ -29,6 +42,14 @@ class MyClass2:
     k: dict[str, str] = {"str": "str"}
 
     def __init__(self):
+        pass
+
+
+class MyAbstractClass():
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def foo(x, y, c):
         pass
 
 
@@ -41,6 +62,13 @@ def function(x, y: float, j: str = "Default", k: dict[str, float] = {"str": 3.33
             def entryentryfunction(x, y, n):
                 pass
 
+    return [[]]
 
-def function2(x, y: float, j: str = "Default", k: dict[str, float] = {"str": 3.33}) -> list[list]:    
+
+def function2(x, y: float, j: str = "Default", k: dict[str, float] = {"str": 3.33}) -> list[list]:
+    pass
+
+
+@decorator
+def decorated_function(x, y, j, la, o):
     pass
